eaf4392b4d7a5d7caaa1510db3e3114fe55e5832
Jason Nixon <jason.nixon@stratasys.com>
2024-06-17 15:46:21 -0500

    Add import materials func declaration

--- src/src.py
+++ src/src.py
@@ -1,7 +1,7 @@
+import json
 import math
 
 STEFAN_BOLTZMANN_CONSTANT = 5.670374419 * 1e-8
-
 
 class Material():
 
@@ -31,5 +31,25 @@ class Material():
         """Thermal diffusivity [sq.m/s]."""
         return self.thermal_conductivity / self.volumetric_heat_capacity  # [m2/s]
 
+
+def calculate_interface_temperature(mat_1:Material, mat_2:Material, temp_1:float, temp_2:float) -> float:
+    
+    num = mat_1.thermal_effusivity * temp_1 + mat_2.thermal_effusivity * temp_2
+
+    den = mat_1.thermal_effusivity + mat_2.thermal_effusivity
+    
+    return num/den
+
+def import_materials(fpath:str) -> dict[Material]:
+    
+    return []
+    
+layer_count = 10
+
+nodes_per_layer = 10    
+
+node_spacing = layer_count/nodes_per_layer
+
+time_spacing = 0
 
 mat = Material('tst', 1000, 0.200, 2000, 180, None, 300)
