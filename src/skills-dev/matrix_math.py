afdbb86ca6fb36cb84974f57d2add31a4499bc0d
Jason Nixon <jason.nixon@stratasys.com>
2024-06-24 10:03:26 -0500

    (learning)

--- src/skills-dev/matrix_math.py
+++ src/skills-dev/matrix_math.py
@@ -2,15 +2,16 @@
 
 import numpy as np
 
-print('\n\n')
-
+# num rows
 M = 3
 
+# num col
 N = M + 1
 
 FLOAT64 = np.float64
 
 # If size is an int, the result is 1D -> 2D requires a tuple.
+# Initialize an M x N matrix (conduction matrix). 
 a = np.zeros(shape=[M, N], dtype=FLOAT64)
 
 # Matrix index is zero based.
@@ -18,12 +19,13 @@ a[0, 0] = 1
 a[1, 1] = 2
 a[2, 2] = 3
 
-print(np.shape(a))
+# Initialize an N x 1 vector (future: temperature in Z).
+b = np.array([1]*N, dtype=FLOAT64)
 
-b = np.array([1]*M, dtype=FLOAT64).reshape([3, 1])
+print(f'shape of a is {a.shape}')
 
-print(np.shape(b))
+print(f'shape of b is {b.shape}')
 
-c = np.dot(a, b.transpose())
+c = np.dot(a, b.T)
 
 print(c)
