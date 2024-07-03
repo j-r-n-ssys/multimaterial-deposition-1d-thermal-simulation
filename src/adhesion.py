f28c593de721beb3df5460402a5be50e95b944b5
Jason Nixon <jason.nixon@stratasys.com>
2024-07-03 13:24:35 -0500

    Add more incomplete functionality

--- src/adhesion.py
+++ src/adhesion.py
@@ -4,39 +4,45 @@ import logging as lg
 import numpy as np
 
 from os.path import basename as get_module_fname
+from types import NoneType
 
 NUMERICAL_TYPES = (int, float)
 
 CELSIUS_TO_KELVIN_OFFSET = 273.15
 
+UNIVERSAL_GAS_CONSTANT = 8.31446261815324 # J/K-mol
+
 
 class WilliamLandelFerryModel():
-    """This model represents the William-Landel-Ferry time-temperature superposition model. 
-    """
+    """This object represents a William-Landel-Ferry time-temperature superposition model."""
 
-    def __init__(self, c_1, c_2, t_ref: float, t_glass: float) -> None:
+    def __init__(self, c_1, c_2, t_ref: float, t_glass: float | None) -> None:
         """_summary_
 
         Args:
             c_1 (_type_): _description_
             c_2 (float): WLF model 
             t_ref (float): Time-temperature superposition reference temperature [degC]. 
-            t_glass (float): Glass transition temperature [degC]
+            t_glass (float | None): Glass transition temperature [degC]
         """
 
         if not isinstance(c_1, NUMERICAL_TYPES) or not isinstance(c_2, NUMERICAL_TYPES):
             raise TypeError('WLF Model empirical constant C1 and C2 must be a numerical type.')
-        elif not isinstance(t_ref, NUMERICAL_TYPES) or not isinstance(t_glass, NUMERICAL_TYPES):
-            raise TypeError('Temperatures T_ref and T_glass mmust be a numerical type.')
+        elif not isinstance(t_ref, NUMERICAL_TYPES) :
+            raise TypeError('Temperature T_ref mmust be a numerical type.')
+        elif not (t_glass is None or isinstance(t_glass, NUMERICAL_TYPES)):
+            raise TypeError('Temperature T_ref mmust be a numerical type or None.')
         elif c_1 <= 0 or c_2 <= 0:
             raise ValueError('WLF Model empirical constants C1 and C2 must be greater than zero.')
         elif t_ref < -CELSIUS_TO_KELVIN_OFFSET:
-            raise ValueError('Temperatures T_ref and T_glass must be greater than absolute zero.')
+            raise ValueError('Reference temperature T_ref must be greater than absolute zero.')
+        elif t_glass is not None and t_glass < -CELSIUS_TO_KELVIN_OFFSET:
+            raise ValueError('Glass transition temperature T_ref must be greater than absolute zero.')
 
         self.c_1 = c_1
         self.c_2 = c_2
         self.t_r = t_ref + CELSIUS_TO_KELVIN_OFFSET
-        self.t_g = t_glass + CELSIUS_TO_KELVIN_OFFSET
+        self.t_g = t_glass + CELSIUS_TO_KELVIN_OFFSET if t_glass is not None else None
 
     def get_shift_factor(self, temp: (float | np.ndarray)) -> (float | np.ndarray):
         """_summary_
@@ -48,10 +54,11 @@ class WilliamLandelFerryModel():
             float  |  np.ndarray: _description_
         """
 
-        if np.max(temp) > self.t_g + 100:
-            lg.warning('Temperature exceeds WLF model validity upper bound.')
-        elif np.min(temp) < self.t_g:
-            lg.warning('Temperature exceeds WLF model validity lower bound.')
+        if self.t_g is not None:
+            if np.max(temp) > self.t_g + 100:
+                lg.warning('Temperature exceeds WLF model validity upper bound.')
+            elif np.min(temp) < self.t_g:
+                lg.warning('Temperature exceeds WLF model validity lower bound.')
 
         temp = temp + CELSIUS_TO_KELVIN_OFFSET
 
@@ -63,7 +70,33 @@ class WilliamLandelFerryModel():
             raise TypeError('Temperature argument must be a numerical type or numerical array.')
 
 
-# wlf_model = lambda C_1, C_2, T_ref, T: -C_1 * (T - T_ref) / (C_2 + (T - T_ref))
+class ArrheniusModel():
+    """This object represents a Arrhenius time-temperature superposition model."""
+    
+    def __init__(self, e_a:float, t_ref:float) -> None:
+        
+    
+        self.e_a = e_a
+        self.t_r = t_ref
+        
+    def get_shift_factor(self, temp: (float | np.ndarray)) -> (float | np.ndarray):
+        """_summary_
+
+        Args:
+            temp (float  |  np.ndarray): Temperature [degC]
+
+        Returns:
+            float  |  np.ndarray: _description_
+        """
+
+        return 10** ((-self.e_a / 2.303 * UNIVERSAL_GAS_CONSTANT) * (1/temp - 1/self.t_r))
 
 if __name__ == '__main__':
-    lg.warning('Module %s is not intended to be run as standalone module.', get_module_fname(__file__))
+    # lg.warning('Module %s is not intended to be run as standalone module.', get_module_fname(__file__))
+
+    print(' ')
+    print(' ')
+
+    wlf = WilliamLandelFerryModel(5.78, 182, 200, None)
+    
+    print(wlf.get_shift_factor(205))
