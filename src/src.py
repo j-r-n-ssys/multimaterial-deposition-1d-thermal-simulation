5456be7759913e2a587593a7a68e5e2bb461dcc0
Jason Nixon <jason.nixon@stratasys.com>
2024-07-03 10:43:11 -0500

    Refactor for pylint

--- src/src.py
+++ src/src.py
@@ -1,3 +1,5 @@
+"""_missing_docstring_""" 
+
 import logging as lg
 import math
 import matplotlib.pyplot as plt
@@ -12,24 +14,6 @@ STEFAN_BOLTZMANN_CONSTANT = 5.670374419 * 1e-8
 FLOAT64 = np.float64
 
 lg.basicConfig(level=lg.INFO)
-
-
-class Later():
-
-    def __init__(self, width: float, height: float):
-        """_summary_
-
-        Args:
-            width (float): [m]
-            height (float): [m]
-        """
-        self.width: float = width
-        self.height: float = height
-
-    @property
-    def area(self) -> float:
-        """Approximate layer area. """
-        return self.width * self.height
 
 
 def inch_to_millimeter(f: float, n: int = 1) -> float:
@@ -49,12 +33,10 @@ def inch_to_millimeter(f: float, n: int = 1) -> float:
     if f == 0:
         raise ValueError('n = 0 has no effect.')
 
-    res = float(f) * (25.4**n)
-
-    return res
+    return float(f) * (25.4**n)
 
 
-def mk_conduction_matrix(M1: Material, M2: Material) -> np.ndarray:
+def mk_conduction_matrix(m_1: Material, m_2: Material) -> np.ndarray:
     """---IN DEVELOPMENT---"""
 
     if LAYER_CNT < 2:
@@ -70,35 +52,35 @@ def mk_conduction_matrix(M1: Material, M2: Material) -> np.ndarray:
     h1 = h2 = h0 / 2
 
     # Store params for convenience.
-    D1 = M1.thermal_diffusivity
-    K1 = M1.thermal_conductivity
-    D2 = M2.thermal_diffusivity
-    K2 = M2.thermal_conductivity
+    d_1 = m_1.thermal_diffusivity
+    k_1 = m_1.thermal_conductivity
+    d_2 = m_2.thermal_diffusivity
+    k_2 = m_2.thermal_conductivity
 
     # Initialize the conduction coefficient matrix.
     coeff = np.zeros(shape=[NODE_CNT, NODE_CNT], dtype=FLOAT64)
 
     # Initialize a convenience array for second order finite difference approximation matrix.
-    ARR = np.array([1, -2, 1], dtype=FLOAT64)
+    fd_arr = np.array([1, -2, 1], dtype=FLOAT64)
 
     for i in range(0, NODE_CNT):
         match i:
             case 0:
-                coeff[i, i:i + 3] = (D1 / h0**2) * ARR  # fwd
+                coeff[i, i:i + 3] = (d_1 / h0**2) * fd_arr  # fwd
 
             case i if i > 0 and i < NODES_PER_LAYER - 1:
-                coeff[i, i - 1:i + 2] = (D1 / h0**2) * ARR
+                coeff[i, i - 1:i + 2] = (d_1 / h0**2) * fd_arr
 
             case i if i == NODES_PER_LAYER - 1:
-                C1, C2 = hickson.jump_match_coeff(K1, D1, K2, D1, h0, h1, h2, CONTACT_TRANSFER_COEFF)
-                coeff[i, i - 1:i + 3] = C1
-                coeff[i + 1, i - 1:i + 3] = C2
+                c_1, c_2 = hickson.jump_match_coeff(k_1, d_1, k_2, d_1, h0, h1, h2, CONTACT_TRANSFER_COEFF)
+                coeff[i, i - 1:i + 3] = c_1
+                coeff[i + 1, i - 1:i + 3] = c_2
 
             case i if i == NODES_PER_LAYER:
                 pass  # This nodes coefficients were already set in i == NODES_PER_LAYER - 1.
 
             case i if i > NODES_PER_LAYER - 2 and i < NODE_CNT - 1:
-                coeff[i, i - 1:i + 2] = (D2 / h0**2) * ARR
+                coeff[i, i - 1:i + 2] = (d_2 / h0**2) * fd_arr
 
             case i if i == NODE_CNT - 1:
                 pass
@@ -110,7 +92,7 @@ def mk_conduction_matrix(M1: Material, M2: Material) -> np.ndarray:
     return coeff
 
 
-def mk_convection_matrix(M1: Material, M2: Material) -> np.array:
+def mk_convection_matrix(m_1: Material, m_2: Material) -> np.array:
     """This function generates a Nx1 matrix of convection coefficients. 
 
     Args:
@@ -121,11 +103,15 @@ def mk_convection_matrix(M1: Material, M2: Material) -> np.array:
         np.array: Convection FD coefficients. 
     """
 
+    cv_1 = m_1.volumetric_heat_capacity
+
+    cv_2 = m_2.volumetric_heat_capacity
+
     coeff = np.zeros(shape=NODE_CNT, dtype=FLOAT64)
 
-    coeff[0:NODES_PER_LAYER - 1] = CONVECTION_COEFF / (M1.volumetric_heat_capacity * DELTA_Z)
+    coeff[0:NODES_PER_LAYER - 1] = CONVECTION_COEFF / (cv_1 * DELTA_Z)
 
-    coeff[NODES_PER_LAYER:-1] = CONVECTION_COEFF / (M2.volumetric_heat_capacity * DELTA_Z)
+    coeff[NODES_PER_LAYER:-1] = CONVECTION_COEFF / (cv_2 * DELTA_Z)
 
     return coeff
 
@@ -145,14 +131,14 @@ def solve_system(M1: Material, M2: Material, temp: np.ndarray) -> np.ndarray:
     # Calculate the time step; see eq. 4 in Basgul1 et al.
     time_step = 0.9 * (0.5 * (DELTA_Z**2) / max(M1.thermal_diffusivity, M2.thermal_diffusivity))
 
-    lg.info(f'time step = {time_step}')
+    lg.info('time step = %s', time_step)
 
     # Calculate the total number of time steps.
     time_steps = int(math.ceil(TIME / time_step))
 
-    lg.info(f'time step cnt = {time_steps}')
+    lg.info('time step cnt = %s', time_steps)
 
-    lg.info(f'simulation time = {float(time_steps) * time_step}')
+    lg.info('simulation time = %s', float(time_steps) * time_step)
 
     # Make the conduction coefficient matrix.
     coeff_k = mk_conduction_matrix(M1, M2)
@@ -160,19 +146,16 @@ def solve_system(M1: Material, M2: Material, temp: np.ndarray) -> np.ndarray:
     # Make the convection coefficient matrix.
     coeff_h = mk_convection_matrix(M1, M2)
 
-    res = np.zeros(shape=[NODE_CNT, time_steps], dtype=FLOAT64)
-
-    # Store the initial temperature profile.
-    T = temp
+    res = np.zeros(shape=[NODE_CNT, time_steps], dtype=FLOAT64) #pylint disable=redefined_outer_scope
 
     # Store the initial temperature state in the result array.
-    res[:, 0] = T
+    res[:, 0] = temp
 
     for i in range(1, time_steps):
 
-        T = res[:, i - 1]
+        temp = res[:, i - 1]
 
-        res[:, i] = T + time_step * (np.dot(coeff_k, T.T) - coeff_h * (T - T_AMB))
+        res[:, i] = temp + time_step * (np.dot(coeff_k, temp.T) - coeff_h * (temp - T_AMB))
 
     return res
 
@@ -190,13 +173,15 @@ LAYER_CNT = 10
 
 NODES_PER_LAYER = 400
 
-lg.info(f'layer thickness = {LAYER_THICKNESS}\nlayer count = {LAYER_CNT}\nnodes per layer = {NODES_PER_LAYER}')
+lg.info('layer thickness = %s',LAYER_THICKNESS)
+lg.info('layer count = %s', LAYER_CNT)
+lg.info('nodes per layer = %s', NODES_PER_LAYER)
 
 NODE_CNT = LAYER_CNT * NODES_PER_LAYER
 
 DELTA_Z = LAYER_THICKNESS / NODES_PER_LAYER
 
-lg.info(f'node spacing = {DELTA_Z}')
+lg.info('node spacing = %s', DELTA_Z)
 
 CONVECTION_COEFF = 50
 
