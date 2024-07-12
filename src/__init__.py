2b682da229a67a3f75d554554dfc08c8222a8817
Jason Nixon <jason.nixon@stratasys.com>
2024-07-12 09:00:40 -0500

    Remoe adhesion module
    This functionality was rolled into the material module, the adhesion model was to-be depricated.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
-
 from hickson import calc_interface_temp
 
 from hickson import cond_match_coeff
 
 from hickson import jump_match_coeff
+
+__all__ = ['calc_interface_temp', 'cond_match_coeff', 'jump_match_coeff']
