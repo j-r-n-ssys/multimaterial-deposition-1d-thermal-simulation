6f440dca2a71b7ab75f8240f7f565e6adcbdf44b
Jason Nixon <jason.nixon@stratasys.com>
2024-08-20 09:41:07 -0500

    Finish custom logging formatter

--- src/common.py
+++ src/common.py
@@ -1,29 +1,25 @@
 """Container for common functions."""
 
 import logging as lg
-import time
 
 from datetime import datetime
 from pathlib import Path
 
-LOGGER_STR_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGGER_STR_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - message: %(message)s'
 
-LOGGER_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+LOGGER_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f%Z'
 
 
-class Formatter(lg.Formatter):
-    """Extension of the logging module formatter with prefered datetime formatting. """
+class CustomFormatter(lg.Formatter):
+    """Logging module formatter with custom time formatter. """
 
     def formatTime(self, record, datefmt=None):  #pylint disable=invalid-name
-        """Datetime formatter."""
-        ct = self.converter(record.created)
-        if datefmt is not None:
-            # support %z and %f in datefmt (struct_time doesn't carry ms or tz)
-            datefmt = datefmt.replace('%f', str(datetime.fromtimestamp(record.created).microsecond).zfill(6))
-            datefmt = datefmt.replace('%z', time.strftime('%z'))
-            s = time.strftime(datefmt, ct)
-        else:
-            t = time.strftime('%Y-%m-%d %H:%M:%S', ct)
+        """Customer datetime formatter."""
+
+        dt = datetime.fromtimestamp(record.created)
+
+        s = dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] if datefmt is None else dt.strftime(datefmt)
+
         return s
 
 
@@ -36,7 +32,7 @@ ch = lg.StreamHandler()
 # create the console handler
 ch.setLevel(lg.DEBUG)
 
-formatter = Formatter(
+formatter = CustomFormatter(
     fmt=LOGGER_STR_FORMAT,
     datefmt=LOGGER_DATETIME_FORMAT,
 )
@@ -48,6 +44,6 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 if __name__ == '__main__':
-    print(f'\nModule <{Path(__file__).name}> is not intended to be run as standalone module.')
+    print(f'Module <{Path(__file__).name}> is not intended to be run as standalone module.')
 
     logger.debug('a')
